1332153f1d51e2a54c7617f87eda7638219ebccc
Tarunsharma9855 <tarunsharma9855@gmail.com>
2024-06-28 13:02:39 +0530

    class

--- class/class.cpp
+++ class/class.cpp
@@ -1,9 +1,30 @@
 #include<iostream>
 using namespace std;
 
+
+class employee {
+    public:
+     void first(){
+        cout<<"hello"<<endl;
+     }
+     void  second(){
+        cout<<"hello2"<<endl;
+     }
+};
+
+class employee2{
+    public :
+     void third(){
+        cout<<"hello third"<<endl;
+        cout<<"hello fourth"<<endl;
+     }
+};
+
 int main(){
-    cout<<"hell";
-    cout <<"hello2";
+    employee t;
+    employee2 s;
+    t.first();
+    s.third();
 
 
     return 0;
