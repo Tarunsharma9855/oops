758cec7f990c503c606ac0ee8f5e55a93df44c24
Tarunsharma9855 <tarunsharma9855@gmail.com>
2024-07-01 12:21:31 +0530

    constructor Overloading

---
+++ constructor/ConsOverloading.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+class constructor{
+    public :
+
+    constructor(int x){
+        cout<<x<<endl;
+    }
+    constructor(int x,int y){
+        cout<<x<<endl<<y<<endl;
+    }
+
+};
+
+
+
+int main(){
+
+    constructor t1(3);
+
+
+    return 0;
+
+}
